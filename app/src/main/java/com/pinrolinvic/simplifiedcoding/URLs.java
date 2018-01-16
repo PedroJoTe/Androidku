e09e1afa5c927fd4ba0484168ec94498627e8b51
pin <pinrolinvic@gmail.com>
2018-01-16 15:52:23 +0100

    initial commit

---
+++ app/src/main/java/com/pinrolinvic/simplifiedcoding/URLs.java
@@ -0,0 +1,13 @@
+package com.pinrolinvic.simplifiedcoding;
+
+/**
+ * Created by helle on 12/27/17.
+ */
+
+public class URLs {
+
+    private static final String ROOT_URL = "http://www.pinrolinvic.com/testy/login3/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+}
